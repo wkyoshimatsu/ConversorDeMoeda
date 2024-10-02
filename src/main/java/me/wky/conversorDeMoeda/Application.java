ca3895cedf10b27c8733eddb7c0fcfc6f7424bb9
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2024-10-02 20:43:43 -0300

    Added maven project files

---
+++ src/main/java/me/wky/conversorDeMoeda/Application.java
@@ -0,0 +1,60 @@
+package me.wky.conversorDeMoeda;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class Application {
+
+    public static final String API_KEY= System.getenv("API_KEY");
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("""
+                ***************************************************
+                      Seja bem-vindo(a) ao Conversor de Moeda
+                
+                1) Real (BRL)             ->  Dólar Americano (USD)
+                2) Dólar Americano (USD)  ->  Real (BRL)
+                3) Real (BRL)             ->  Peso Argentino (ARS)
+                4) Peso Argentino (ARS)   ->  Real (BRL)
+                5) Real (BRL)             ->  Iene (JPY)
+                6) Iene (JPY)             ->  Real (BRL)
+                7) Sair
+                Selecione uma opção válida:
+                ***************************************************""");
+
+        Scanner scanner =
+
+        // Setting URL
+        String url_str = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/latest/USD";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url_str)).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+        /*
+
+
+
+
+        // Making Request
+        URL url = new URL(url_str);
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+        request.connect();
+
+        // Convert to JSON
+        JsonParser jp = new JsonParser();
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+        JsonObject jsonobj = root.getAsJsonObject();
+
+        // Accessing object
+        String req_result = jsonobj.get("result").getAsString();
+
+        */
+    }
+}
