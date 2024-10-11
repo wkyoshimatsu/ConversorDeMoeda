7a016597c72be74960eb92a3133c4ebf289d5d69
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2024-10-11 14:16:35 -0300

    Added ConversionFromBaseToTargetCurrency classes

--- src/main/java/me/wky/conversorDeMoeda/model/conversion/ConversionFromBaseToTargetCurrencyExchangeRate.java
+++ src/main/java/me/wky/conversorDeMoeda/model/conversion/ConversionFromBaseToTargetCurrencyExchangeRate.java
@@ -1,4 +1,4 @@
-package me.wky.conversorDeMoeda.models;
+package me.wky.conversorDeMoeda.model.conversion;
 
 public record ConversionFromBaseToTargetCurrencyExchangeRate(String base_code, String target_code, float conversion_rate) {
 }
