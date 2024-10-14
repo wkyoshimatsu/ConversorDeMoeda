3fa159c448d261496c322a0d021a6c9b65767d37
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2024-10-14 17:48:09 -0300

    refactor: Refactored main method

--- src/main/java/me/wky/conversorDeMoeda/Application.java
+++ src/main/java/me/wky/conversorDeMoeda/Application.java
@@ -5,6 +5,8 @@ import me.wky.conversorDeMoeda.model.ScreenMessage;
 import me.wky.conversorDeMoeda.model.conversion.ConversionFromBaseCurrency;
 import me.wky.conversorDeMoeda.model.conversion.ConversionFromBaseToTargetCurrency;
 import me.wky.conversorDeMoeda.util.ConversionDeserializer;
+import me.wky.conversorDeMoeda.util.HttpResponseFactory;
+import me.wky.conversorDeMoeda.util.exchangeRateApi.ExchangeRateAPI;
 
 import java.io.IOException;
 import java.net.URI;
@@ -18,29 +20,6 @@ import java.util.Scanner;
 public class Application {
 
     public static final String API_KEY= System.getenv("API_KEY");
-
-
-
-    public static double getValueToConvert(){
-        Scanner scanner = new Scanner(System.in);
-
-        double valueToConvert = 0;
-        while (valueToConvert <= 0){
-            System.out.println("Digite o valor que deseja converter:");
-            valueToConvert = scanner.nextDouble();
-        }
-        return valueToConvert;
-    }
-
-    public static HttpResponse<String> getHttpResponseByUrl(String url_str) throws IOException, InterruptedException{
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url_str))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-        return response;
-    }
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
@@ -56,83 +35,135 @@ public class Application {
 
         int option = 0;
         while (true){
+            String baseCurrency;
+            String targetCurrency;
+            double valueToConvert;
+            float conversionRate;
+
             ScreenMessage.printExchangeOptions();
             option = InputValidator.exchangeSelector();
 
-            if (option == 7){
+
+            if (option == ScreenMessage.EXIT_OPTION){
+
+                ScreenMessage.printEndMessage();
                 break;
+
+            } else if (option != ScreenMessage.CUSTOM_OPTION){
+
+                baseCurrency = ScreenMessage.getBaseCurrency(option);
+                targetCurrency = ScreenMessage.getTargetCurrency(option);
+
+                valueToConvert = InputValidator.getValueToConvert();
+
+                ExchangeRateAPI exchangeRateAPI = new ExchangeRateAPI(baseCurrency, targetCurrency);
+
+                String json = HttpResponseFactory
+                        .getJsonByUrl(exchangeRateAPI
+                                .getUrlWithBaseAndTargetCurrency());
+
+                ConversionFromBaseToTargetCurrency conversionFromBaseToTargetCurrency = ConversionDeserializer
+                        .deserializeToConversionFromBaseToTargetCurrency(json);
+
+                conversionRate = conversionFromBaseToTargetCurrency.getConversionRate();
+
+                ScreenMessage.printExchangeResult(valueToConvert,
+                        baseCurrency,
+                        valueToConvert * conversionRate,
+                        targetCurrency);
+            } else {
+
+
+
+
+
+
+
+                baseCurrency = ScreenMessage.getBaseCurrency(option);
+                targetCurrency = ScreenMessage.getTargetCurrency(option);
+
+                valueToConvert = InputValidator.getValueToConvert();
+
+                ExchangeRateAPI exchangeRateAPI = new ExchangeRateAPI(baseCurrency, targetCurrency);
+
+                String json = HttpResponseFactory
+                        .getJsonByUrl(exchangeRateAPI
+                                .getUrlWithBaseCurrency());
+
+                ConversionFromBaseCurrency conversionFromBaseCurrency = ConversionDeserializer
+                        .deserializeToConversionFromBaseCurrency(json);
+
+                Map<Integer, String> baseCodeOptions = new HashMap<>();
+
+                int index = 1;
+                for (String baseCode : conversionFromBaseCurrency.getConversionRates().keySet()) {
+                    baseCodeOptions.put(index, baseCode);
+                    index++;
+                }
+
+                int baseCodeOption = 0;
+                while (true){
+                    System.out.println("Selecione a moeda base:");
+                    for (int i = 1; i <= baseCodeOptions.size(); i++) {
+                        System.out.printf("%d) %s\n", i, baseCodeOptions.get(i));
+                    }
+                    baseCodeOption = new Scanner(System.in).nextInt();
+                    if (baseCodeOptions.containsKey(baseCodeOption)){
+                        break;
+                    }
+                }
+
+                String selectedBaseCode = baseCodeOptions.get(baseCodeOption);
+
+                json = HttpResponseFactory
+                        .getJsonByUrl("https://v6.exchangerate-api.com/v6/" + API_KEY + "/pair/" + selectedBaseCode + "/" + targetCurrency);
+
+                ConversionFromBaseToTargetCurrency conversionFromBaseToTargetCurrency = ConversionDeserializer
+                        .deserializeToConversionFromBaseToTargetCurrency(json);
+
+                conversionRate = conversionFromBaseToTargetCurrency.getConversionRate();
+
+                ScreenMessage.printExchangeResult(valueToConvert,
+                        selectedBaseCode,
+                        valueToConvert * conversionRate,
+                        targetCurrency);
+
+
+
+                //
+
+                url_str = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/latest/BRL";
+
+                response = getHttpResponseByUrl(url_str);
+
+                json = response.body();
+
+                //System.out.println(json);
+
+                ConversionFromBaseCurrency conversionFromBaseCurrency = ConversionDeserializer.deserializeToConversionFromBaseCurrency(json);
+
+                //System.out.println(conversionFromBaseCurrency.getConversionRates());
+
+                Map<Integer, String> baseCodeOptions = new HashMap<>();
+
+                int index = 1;
+                for (String baseCode : conversionFromBaseCurrency.getConversionRates().keySet()) {
+                    baseCodeOptions.put(index, baseCode);
+                    index++;
+                }
+
+                //System.out.println(baseCodeOptions);
+
+
+
+
+
+
+
+                //gson = new Gson();
+                //ConversionExchangeRate conversion = gson.fromJson(json, ConversionExchangeRate.class);
             }
 
-            String baseCurrency = ScreenMessage.getBaseCurrency(option);
-            String targetCurrency = ScreenMessage.getTargetCurrency(option);
         }
-
-
-
-
-
-
-
-        while (){
-
-
-
-
-            if (option == 7){
-                break;
-            }
-
-            double valueToConvert = getValueToConvert();
-
-            String url_str = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/pair/" + baseCurrency + "/" + targetCurrency;
-
-            HttpResponse<String> response = getHttpResponseByUrl(url_str);
-
-            String json = response.body();
-
-            //System.out.println(json);
-
-            ConversionFromBaseToTargetCurrency conversionFromBaseToTargetCurrency = ConversionDeserializer.deserializeToConversionFromBaseToTargetCurrency(json);
-
-            float conversionRate = conversionFromBaseToTargetCurrency.getConversionRate();
-
-            System.out.printf("Valor %.2f [%s] corresponde ao valor final de -> %.2f [%s].\n", valueToConvert, baseCurrency, valueToConvert * conversionRate, targetCurrency);
-
-            url_str = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/latest/BRL";
-
-            response = getHttpResponseByUrl(url_str);
-
-            json = response.body();
-
-            //System.out.println(json);
-
-            ConversionFromBaseCurrency conversionFromBaseCurrency = ConversionDeserializer.deserializeToConversionFromBaseCurrency(json);
-
-            //System.out.println(conversionFromBaseCurrency.getConversionRates());
-
-            Map<Integer, String> baseCodeOptions = new HashMap<>();
-
-            int index = 1;
-            for (String baseCode : conversionFromBaseCurrency.getConversionRates().keySet()) {
-                baseCodeOptions.put(index, baseCode);
-                index++;
-            }
-
-            //System.out.println(baseCodeOptions);
-
-
-
-
-
-
-
-            //gson = new Gson();
-            //ConversionExchangeRate conversion = gson.fromJson(json, ConversionExchangeRate.class);
-
-
-        }
-
-
-
     }
 }
