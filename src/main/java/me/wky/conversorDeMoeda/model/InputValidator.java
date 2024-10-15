18f43dd3a42bc1bc8e532867ed2f34ffe90ae19e
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2024-10-14 23:01:01 -0300

    refactor: Fully implemented getCustomBaseCurrency and getCustomTargetCurrency

--- src/main/java/me/wky/conversorDeMoeda/model/InputValidator.java
+++ src/main/java/me/wky/conversorDeMoeda/model/InputValidator.java
@@ -1,5 +1,6 @@
 package me.wky.conversorDeMoeda.model;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class InputValidator {
@@ -25,17 +26,32 @@ public class InputValidator {
         return valueToConvert;
     }
 
-    public static String getCustomBaseCurrency(int option){
+    public static String getCustomBaseCurrency(List<String> currencies){
         Scanner scanner = new Scanner(System.in);
 
-        String baseCurrency;
-        while (true){
-            ScreenMessage.printAllCurrencyCodes();
-            baseCurrency = scanner.nextLine().toUpperCase();
-            if (Currency.STANDARD_CURRENCIES.containsKey(baseCurrency)){
-                break;
+        int option = 0;
+        while (option < 1 || option > currencies.size() + 1){
+            System.out.println("Selecione a moeda base:");
+            ScreenMessage.printAllCurrencyCodes(currencies);
+            option = scanner.nextInt();
+        }
+        return currencies.get(option - 1);
+    }
+
+    public static String getCustomTargetCurrency(String customBaseCurrency, List<String> currencies){
+        Scanner scanner = new Scanner(System.in);
+
+        int option = 0;
+        while (option < 1 || option > currencies.size() + 1){
+            System.out.println("Selecione a moeda de destino:");
+            ScreenMessage.printAllCurrencyCodes(currencies);
+            option = scanner.nextInt();
+
+            if (currencies.get(option - 1).equals(customBaseCurrency)){
+                System.out.println("A moeda de destino não pode ser a mesma que a moeda base.");
+                option = 0;
             }
         }
-        return baseCurrency;
+        return currencies.get(option - 1);
     }
 }
